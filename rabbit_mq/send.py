771de647df680bf9d9be2e59d0710fab5acba937
caribbeandy <a9palmer@ugster08.student.cs.uwaterloo.ca>
2017-07-23 15:25:04 -0400

    Load messages into memory first

--- rabbit_mq/send.py
+++ rabbit_mq/send.py
@@ -11,17 +11,21 @@ params = pika.ConnectionParameters(
 	credentials=credentials
 	)
 connection = pika.BlockingConnection(params)
-	#connection = pika.BlockingConnection(pika.ConnectionParameters(host='ugster09.student.cs.uwaterloo.ca'))
 
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-for x in range(0, 10000):
-	body = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+messages = []
+
+for x in range(0, 100000):
+	msg = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+	messages.append(msg)
+
+for i in range(len(messages)):
 	channel.basic_publish(exchange='',
 			      routing_key='hello',
-			      body=body)
+			      body=messages[i])
 
 #print ("SENT", body)
 connection.close()
