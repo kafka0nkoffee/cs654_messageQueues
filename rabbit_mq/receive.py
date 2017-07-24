d8637ddceac0ce9c74bc49afdce3b89647129e8e
caribbeandy <a9palmer@ugster08.student.cs.uwaterloo.ca>
2017-07-24 10:55:25 -0400

    Print receive times

--- rabbit_mq/receive.py
+++ rabbit_mq/receive.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 import pika
+import calendar
+import time
+import sys
+
+msgId=0
+f = open('receive.log', 'w')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
@@ -7,11 +13,34 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
-    print(body)
+	global msgId
+	global times
+	global f
+
+	times.append([msgId,calendar.timegm(time.gmtime())])
+	msgId+=1
+
+	# Stopping conditions
+	if msgId==10000:
+
+		print('Received ' + str(len(times)) + ' messages')
+		for item in times:
+			
+			msgId = item[0]
+			receiveTime = item[1] 
+			f.write(str(msgId))
+			f.write(" ")
+			f.write(str(receiveTime))
+			f.write("\n")
+
+		print("Done consuming.")
+		sys.exit()
 
 channel.basic_consume(callback,
                       queue='hello',
                       no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
+
+times = []
 channel.start_consuming()
